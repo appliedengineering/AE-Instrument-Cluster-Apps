8cd95cd1b0e51a09b0530033fbe577de5bebe909
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-10-12 23:06:48 -0700

    Create project

---
+++ IOS-inst-clstr/IOS-inst-clstrTests/IOS_inst_clstrTests.swift
@@ -0,0 +1,33 @@
+//
+//  IOS_inst_clstrTests.swift
+//  IOS-inst-clstrTests
+//
+//  Created by Richard Wei on 10/12/20.
+//
+
+import XCTest
+@testable import IOS_inst_clstr
+
+class IOS_inst_clstrTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
