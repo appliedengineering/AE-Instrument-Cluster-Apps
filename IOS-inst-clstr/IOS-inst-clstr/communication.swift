ed02aa3240efedd788a8abc973d60c6afcc9d401
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-10-20 20:48:33 -0700

    Create classes

---
+++ IOS-inst-clstr/IOS-inst-clstr/communication.swift
@@ -0,0 +1,14 @@
+//
+//  communication.swift
+//  IOS-inst-clstr
+//
+//  Created by Richard Wei on 10/20/20.
+//
+
+import Foundation
+import UIKit
+import SwiftyZeroMQ5
+
+class communicationClass{
+    
+}
---
+++ IOS-inst-clstr/IOS-inst-clstr/msgpack.swift
@@ -0,0 +1,14 @@
+//
+//  msgpack.swift
+//  IOS-inst-clstr
+//
+//  Created by Richard Wei on 10/20/20.
+//
+
+import Foundation
+import UIKit
+import MessagePack
+
+class msgpackClass{
+    
+}
