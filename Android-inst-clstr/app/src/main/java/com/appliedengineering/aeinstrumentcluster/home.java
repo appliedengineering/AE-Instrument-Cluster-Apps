436548035bdad0b634bff49a81dda798e6fb15d6
richy6 <richardwei6@gmail.com>
2020-11-29 22:30:46 -0800

    Added support for dark mode

--- Android-inst-clstr/app/src/main/java/com/appliedengineering/aeinstrumentcluster/home.java
+++ Android-inst-clstr/app/src/main/java/com/appliedengineering/aeinstrumentcluster/home.java
@@ -2,17 +2,33 @@ package com.appliedengineering.aeinstrumentcluster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class home extends AppCompatActivity {
 
+    private Boolean isSystemDarkMode(){
+        switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                return true;
+            case Configuration.UI_MODE_NIGHT_NO:
+                return false;
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setTheme(isSystemDarkMode() ? R.style.DarkTheme : R.style.LightTheme);
+        setContentView(R.layout.home_layout);
+        System.out.println(" is dark mode - " + isSystemDarkMode());
     }
+
 
 }
