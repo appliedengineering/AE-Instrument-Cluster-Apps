2970935b2ae2f1aa06a17b35ff0043fee4a49104
richy6 <richardwei6@gmail.com>
2020-11-29 16:44:03 -0800

    Set android icon as ae logo

---
+++ Android-inst-clstr/app/src/main/java/com/appliedengineering/aeinstrumentcluster/home.java
@@ -0,0 +1,18 @@
+package com.appliedengineering.aeinstrumentcluster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
