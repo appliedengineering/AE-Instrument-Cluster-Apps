59fcd1d99dd6e152d5fd708cba3004117551ce88
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2020-12-30 22:48:18 -0800

    Finished communication class

--- Android-inst-clstr/app/src/main/java/com/appliedengineering/aeinstrumentcluster/communication/communication.java
+++ Android-inst-clstr/app/src/main/java/com/appliedengineering/aeinstrumentcluster/communication/communication.java
@@ -1,11 +1,14 @@
-package com.appliedengineering.aeinstrumentcluster.communication;
+package com.appliedengineering.aeinstrumentcluster.Backend;
 import org.zeromq.ZMQ;
 import org.zeromq.SocketType;
+import org.zeromq.ZMQException;
 
 public final class communication {
 
     private static ZMQ.Context ctx;
     private static ZMQ.Socket dish;
+    private static String connectionString = "";
+    private static String group = "";
 
     private communication(){} // private constructor
 
@@ -19,9 +22,45 @@ public final class communication {
         ctx.close();
     }
 
-    public static boolean connect(){
-        
+    public static boolean connect(String connectionStr, String connectionGroup, int recvReconnect, int recvBuffer){
+        connectionString = connectionStr;
+        group = connectionGroup;
+        try {
+            dish.bind(connectionString);
+            dish.join(group);
+            dish.setReceiveTimeOut(recvReconnect);
+            dish.setReceiveBufferSize(recvBuffer);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
         return true;
+    }
+
+    public static boolean disconnect(){
+        try{
+            dish.leave(group);
+            dish.disconnect(connectionString);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean newConnection(String connectionStr, String connectionGroup, int recvReconnect, int recvBuffer){
+        if (!disconnect()){
+            System.out.println("Failed disconnect but not severe error");
+        }
+
+        if (!connect(connectionStr, connectionGroup, recvReconnect, recvBuffer)){
+            return false;
+        }
+        return true;
+    }
+
+    public static String convertErrno(int errorn){
+        return ZMQ.Error.findByCode(errorn).getMessage();
     }
 
 }
