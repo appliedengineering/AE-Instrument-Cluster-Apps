6e972156fee010f25cb6218cd447854a3b39cf44
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2020-12-31 19:37:20 -0800

    Finished adding scrollview fragment

---
+++ Android-inst-clstr/app/src/main/java/com/appliedengineering/aeinstrumentcluster/UI/home_content_scroll.java
@@ -0,0 +1,25 @@
+package com.appliedengineering.aeinstrumentcluster.UI;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appliedengineering.aeinstrumentcluster.R;
+
+public class home_content_scroll extends Fragment {
+    public home_content_scroll(){
+        super(R.layout.home_content_scroll_layout);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+
+
+        return view;
+    }
+
+}
